c35c0ee8d23dae386b5e6ad13a93c087f389e3bd
ibbya42 <ibbyali4214@gmail.com>
2023-10-02 21:53:24 +1300

    2/10 Question 4 Push

---
+++ Questions 1-10/app.js
@@ -0,0 +1,33 @@
+// Question 1
+// Return the sum of two numbers 
+
+// function hoursintoSeconds ( num1 , num2){
+//     // Need to use return if you want to use the value
+//     return num1 + num2
+// }
+
+// console.log(hoursintoSeconds(3,5))
+
+// Question 2
+// Hours to seconds
+
+// function hoursintoSeconds (hour){
+//     // Need to use return if you want to use the value
+//     return hour * 60 * 60
+// }
+
+// console.log(hoursintoSeconds(2))
+
+
+// Question 3
+// Perimeter
+
+// function calcPerimeter (length , width){
+//     return (length + width) * 2;
+// }
+
+// console.log(calcPerimeter(6,7))
+
+// Question 4
+// Area of a Triangle
+
