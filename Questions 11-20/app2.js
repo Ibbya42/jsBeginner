db7c85b8cd900bbdd3e3a4c7f7d6f48f5754197b
ibbya42 <ibbyali4214@gmail.com>
2023-10-03 23:17:55 +1300

    03/10 Question 13 Push

--- Questions 11-20/app2.js
+++ Questions 11-20/app2.js
@@ -13,9 +13,19 @@
 // Question 12
 // If even return 1, else return -1
 
-function booleanInteger(num1){
-    return num1 % 2 === 0 ? 1 : -1
+// function booleanInteger(num1){
+//     return num1 % 2 === 0 ? 1 : -1
+// }
+
+// console.log(booleanInteger(1))
+// console.log(booleanInteger(2))
+
+// Question 13
+// Logged in and Subscribed 
+
+function isLoggedInAndSubscribed(LoginStaus , SubscriptionStatus){
+    return (LoginStaus === 'LOGGED_IN' && SubscriptionStatus === 'SUBSCRIBED')   
 }
 
-console.log(booleanInteger(1))
-console.log(booleanInteger(2))
+console.log(isLoggedInAndSubscribed('LOGGED_IN', 'SUBSCRIBED'))
+console.log(isLoggedInAndSubscribed('LOGGED_IN', 'UNSUBCRIBED'))
