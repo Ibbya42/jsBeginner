89327f026cb60c36f94fa98bd8b734c3aa8babdd
ibbya42 <ibbyali4214@gmail.com>
2023-10-03 22:55:20 +1300

    03/10 Question 12 Push

---
+++ Questions 11-20/app2.js
@@ -0,0 +1,21 @@
+
+// Question 11
+// Odd Number
+
+// function isOdd (num1){
+//     return num1 % 2 !== 0
+// }
+
+// console.log(isOdd(1))
+// console.log(isOdd(2))
+
+
+// Question 12
+// If even return 1, else return -1
+
+function booleanInteger(num1){
+    return num1 % 2 === 0 ? 1 : -1
+}
+
+console.log(booleanInteger(1))
+console.log(booleanInteger(2))
